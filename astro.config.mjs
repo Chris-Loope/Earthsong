7f5113d6e14ad18f2fdd8a5f932451d275ec45ad
Chris Loope <chris@pedagogue.co>
2025-09-06 10:13:46 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
--import { defineConfig } from 'astro/config';
--import tailwind from '@astrojs/tailwind';
--import mdx from '@astrojs/mdx';
-+import { defineConfig } from 'astro/config';
-+import tailwind from '@astrojs/tailwind';
-+import mdx from '@astrojs/mdx';
-+import react from '@astrojs/react';
-
- export default defineConfig({
-   site: 'https://earthsong.io',
--  output: 'static',
--  integrations: [tailwind(), mdx()],
-+  output: 'static',
-+  integrations: [tailwind(), mdx(), react()],
-   vite: {
-     resolve: {
-       alias: { '@': new URL('./src', import.meta.url).pathname }
-     }
-   }
- });
-
-
+export default defineConfig({
+  site: 'https://earthsong.io',
+  output: 'static',
+  integrations: [tailwind(), mdx()],
+  vite: {
+    resolve: { alias: { '@': new URL('./src', import.meta.url).pathname } }
+  }
 });
